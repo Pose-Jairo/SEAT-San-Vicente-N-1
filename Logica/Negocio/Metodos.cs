7dac31d5f74d949201f2055d95aaae533f8d1240
Jairo Pose <megajairopose@hotmail.com>
2023-09-18 13:33:56 -0300

    Conexión con la BD

---
+++ Logica/Negocio/Metodos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Datos.Conexion;
+
+namespace Logica.Negocio
+{
+    public class Metodos
+    {
+        ConexionConBD BDConexion = new ConexionConBD();
+
+        public DataTable Actualizar(DataTable Tabla, string consulta)
+        {
+            Tabla = new DataTable();
+
+            Tabla.Clear();
+
+            Tabla.Load(BDConexion.Lector(consulta));
+
+            BDConexion.Desconectar();
+
+            return (Tabla);
+        }
+
+
+
+
+    }
+}
--- Presentacion/Formularios/MatriculaFrm.cs
+++ Presentacion/Formularios/MatriculaFrm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Logica.Negocio;
 
 namespace Presentacion.Formularios
 {
@@ -14,6 +15,17 @@ namespace Presentacion.Formularios
         public MatriculaFrm()
         {
             InitializeComponent();
+        }
+
+        Metodos metodos = new Metodos();
+        DataTable Tabla = new DataTable();
+
+        string consulta = "SELECT * FROM Estudiante";
+
+        private void MatriculaFrm_Load(object sender, EventArgs e)
+        {
+            dgvEstudiantes.DataSource = metodos.Actualizar(Tabla, consulta);
+            dgvEstudiantes.ClearSelection();
         }
     }
 }
